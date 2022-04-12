53cca5299a54f497684ec6ac15eca9ad2744d63f
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-04-12 19:41:53 -0400

    Update tables_views_ctes.sql

--- classnotes/tables_views_ctes.sql
+++ classnotes/tables_views_ctes.sql
@@ -11,5 +11,18 @@ create table female_alcohol_survey as
 drop table female_alcohol_survey;
 
 create view female_alcohol_survey as
-    select * from alcohol where sex = 'F'
+    select * from alcohol where sex = 'F' --views only store the code while tables store all of that data as a copy.
 ;
+
+select distinct famsize from alcohol;
+
+create view gt3famsize as
+    select * from alcohol 
+    where famsize = 'GT3';
+
+create view le3famsize as
+    select * from alcohol 
+    where famsize = 'LE3';
+
+select avg(dailyalc) from gt3famsize;
+select avg(dailyalc) from le3famsize;
